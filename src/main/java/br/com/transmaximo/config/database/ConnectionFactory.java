2e12110723f27ee50655f3a4e0d2e42522efcbae
DaniloLima345 <86997053+DaniloLima345@users.noreply.github.com>
2022-07-01 01:24:06 -0300

    Criacao da classe para ligacao com o banco de dados

---
+++ src/main/java/br/com/transmaximo/config/database/ConnectionFactory.java
@@ -0,0 +1,25 @@
+package br.com.transmaximo.config.database;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+	
+	public DataSource dataSource;
+	
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/trans-maximo?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("");
+		
+		this.dataSource = comboPooledDataSource;
+	}
+	
+	public Connection recuperarDados() throws SQLException {
+		return this.dataSource.getConnection();
+	}
+
+}
